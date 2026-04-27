b839d95525930cb4c9040e86d70a72d06f99b5b4
Tavares <tavares.gabriel.dev@gmail.com>
2026-04-27 10:00:00 -0300

    Refactor List component to improve task display and interaction
    - Update task rendering to conditionally show completion button based on task status.
    - Add visual feedback for completed tasks with line-through styling.
    - Enhance button styles for better user experience.

--- app/components/List.tsx
+++ app/components/List.tsx
@@ -22,27 +22,35 @@ export default function List({
       <ul className="flex flex-col gap-6">
         {tasks.map((task) => (
           <li
-          key={task.id}
-          className="flex w-full items-center justify-between rounded-lg bg-gray-800 px-4 py-3 text-white shadow-md"
-        >
-          <button
-            onClick={() => handleToggleTaskCompletion(task.id)}
-            className="rounded px-4 text-white"
+            key={task.id}
+            className="flex w-full items-center justify-between rounded-lg bg-gray-800 px-4 py-3 text-white shadow-md"
           >
-            {task.completed ? <span className=" px-2 py-2 rounded bg-green-500">✅</span> : <span className=" px-2 py-2 rounded bg-red-500">❌</span>}
-          </button>
-        
-          <span className="mx-4 text-center flex-1">
-            {task.content}
-          </span>
-        
-          <button
-            onClick={() => handleDeleteTask(task.id)}
-            className="rounded bg-red-500 px-4 text-white"
-          >
-            Apagar
-          </button>
-        </li>
+            <div className="w-16">
+              {!task.completed && (
+                <button
+                  onClick={() => handleToggleTaskCompletion(task.id)}
+                  className="rounded bg-green-500 px-3 py-2 text-white"
+                >
+                  ✅
+                </button>
+              )}
+            </div>
+
+            <span
+              className={`mx-4 flex-1 text-center ${
+                task.completed ? "line-through text-gray-400" : ""
+              }`}
+            >
+              {task.content}
+            </span>
+
+            <button
+              onClick={() => handleDeleteTask(task.id)}
+              className="rounded bg-red-500 px-4 text-white"
+            >
+              Apagar
+            </button>
+          </li>
         ))}
       </ul>
     </div>
